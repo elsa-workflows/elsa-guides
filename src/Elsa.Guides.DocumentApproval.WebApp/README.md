c83da0b7caf46e766185149b470e803d99a02fd1
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2019-09-19 17:59:03 +0200

    Add Elsa.Guides.DocumentApproval.WebApp

---
+++ src/Elsa.Guides.DocumentApproval.WebApp/README.md
@@ -0,0 +1,3 @@
+# Document Approval Web App
+
+This is the code output from https://elsa-workflows.github.io/elsa-core/docs/guides-document-approval
---
+++ src/Elsa.Guides.DocumentApproval.WebApp/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Elsa.Activities.Email.Extensions;
+using Elsa.Activities.Http.Extensions;
+using Elsa.Activities.Timers.Extensions;
+using Elsa.Extensions;
+using Elsa.Persistence.Memory;
+using Elsa.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Elsa.Guides.DocumentApproval.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddWorkflows()
+                .AddHttpActivities(options => options.Bind(Configuration.GetSection("Http")))
+                .AddEmailActivities(options => options.Bind(Configuration.GetSection("Smtp")))
+                .AddTimerActivities(options => options.Bind(Configuration.GetSection("BackgroundRunner")))
+                .AddMemoryWorkflowDefinitionStore()
+                .AddMemoryWorkflowInstanceStore();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IWorkflowRegistry workflowRegistry)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseHttpActivities();
+            app.UseWelcomePage();
+            
+            workflowRegistry.RegisterWorkflow<DocumentApprovalWorkflow>();
+        }
+    }
+}
