f4cfefbb75d65f862d421429e38dbbc29d156da5
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2019-09-19 17:32:13 +0200

    Add Rtl.Guides.HelloWorld.WebApp

---
+++ Rtl.Guides.HelloWorld.WebApp/HelloWorldHttpWorkflow.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using Elsa.Activities.Http.Activities;
+using Elsa.Expressions;
+using Elsa.Services;
+using Elsa.Services.Models;
+
+namespace Rtl.Guides.HelloWorld.WebApp
+{
+    public class HelloWorldHttpWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder)
+        {
+            builder
+                .StartWith<HttpRequestEvent>(
+                    x =>
+                    {
+                        x.Method = HttpMethod.Get.Method;
+                        x.Path = new Uri("/hello-world", UriKind.Relative);
+                    }
+                )
+                .Then<HttpResponseAction>(
+                    x =>
+                    {
+                        x.Content = new LiteralExpression("<h1>Hello World!</h1>");
+                        x.ContentType = "text/html";
+                        x.StatusCode = HttpStatusCode.OK;
+                        x.ResponseHeaders = new LiteralExpression("X-Powered-By=Elsa Workflows");
+                    }
+                );
+        }
+    }
+}
---
+++ Rtl.Guides.HelloWorld.WebApp/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Elsa.Activities.Http.Extensions;
+using Elsa.Extensions;
+using Elsa.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Rtl.Guides.HelloWorld.WebApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddWorkflows()
+                .AddHttpActivities();
+        }
+
+        public void Configure(IApplicationBuilder app, IWorkflowRegistry workflowRegistry)
+        {
+            app.UseHttpActivities();
+            workflowRegistry.RegisterWorkflow<HelloWorldHttpWorkflow>();
+        }
+    }
+}
