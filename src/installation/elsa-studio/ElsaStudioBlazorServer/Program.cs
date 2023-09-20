88ff2dfbca40f2303d88ff1257288ac491b03f9f
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-09-20 20:30:40 +0200

    Add Elsa Studio BlazorServer installation guide

---
+++ src/installation/elsa-studio/ElsaStudioBlazorServer/Program.cs
@@ -0,0 +1,56 @@
+using Elsa.Studio.Backend.Extensions;
+using Elsa.Studio.Core.BlazorServer.Extensions;
+using Elsa.Studio.Dashboard.Extensions;
+using Elsa.Studio.Login.BlazorServer.Extensions;
+using Elsa.Studio.Shell.Extensions;
+using Elsa.Studio.Workflows.Extensions;
+using Elsa.Studio.Workflows.Designer.Extensions;
+
+// Build the host.
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+// Register Razor services.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor(options =>
+{
+    // Register the root components.
+    options.RootComponents.RegisterCustomElsaStudioElements();
+});
+
+// Register shell services and modules.
+builder.Services.AddCore();
+builder.Services.AddShell(options => configuration.GetSection("Shell").Bind(options));
+builder.Services.AddRemoteBackendModule(options => configuration.GetSection("Backend").Bind(options));
+builder.Services.AddLoginModule();
+builder.Services.AddDashboardModule();
+builder.Services.AddWorkflowsModule();
+
+// Configure SignalR.
+builder.Services.AddSignalR(options =>
+{
+    // Set MaximumReceiveMessageSize:
+    options.MaximumReceiveMessageSize = 5 * 1024 * 1000; // 5MB
+});
+
+// Build the application.
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseResponseCompression();
+    
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+// Run the application.
+app.Run();
