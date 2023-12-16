41a36f8f464e6358abb99b911b78ccd72683519b
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2023-12-16 10:34:53 +0100

    Add new project files for ElsaWeb project
    This commit includes the addition of several new files to set up the ElsaWeb project. It consists of configuration files, project file, a basic HTTP workflow along with its execution setup in Program.cs, and a new controller to run workflows.

---
+++ src/installation/elsa-web/ElsaWeb/Controllers/RunWorkflowController.cs
@@ -0,0 +1,19 @@
+using Elsa.Http;
+using Elsa.Workflows.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElsaWeb.Controllers;
+
+[ApiController]
+[Route("run-workflow")]
+public class RunWorkflowController(IWorkflowRunner workflowRunner) : ControllerBase
+{
+    [HttpGet]
+    public async Task Get()
+    {
+        await workflowRunner.RunAsync(new WriteHttpResponse
+        {
+            Content = new("Hello ASP.NET world!")
+        });
+    }
+}
---
+++ src/installation/elsa-web/ElsaWeb/Program.cs
@@ -0,0 +1,21 @@
+using Elsa.Extensions;
+using ElsaWeb.Workflows;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddElsa(elsa =>
+{
+    elsa.AddWorkflow<HttpHelloWorld>();
+    elsa.UseHttp(http => http.ConfigureHttpOptions = options => options.BasePath = "/workflows");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.UseWorkflows();
+app.Run();
