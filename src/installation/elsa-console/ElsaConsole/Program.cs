eb736988273bf48b2db3f477823d2f9550e64a9a
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-01-20 20:38:08 +0100

    Add ElsaConsole project and main program file
    A new ElsaConsole project has been created, which notably includes the package "Elsa" as a dependency. Additionally, a Program.cs file has been added. This establishes a basic setup service container, adds Elsa services to it, builds the container, and includes a simple workflows set to run "Hello World!" and "We can do more than a one-liner!" messages.

---
+++ src/installation/elsa-console/ElsaConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using Elsa.Extensions;
+using Elsa.Workflows.Activities;
+using Elsa.Workflows.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+
+// Setup service container.
+var services = new ServiceCollection();
+
+// Add Elsa services to the container.
+services.AddElsa();
+
+// Build the service container.
+var serviceProvider = services.BuildServiceProvider();
+
+// Instantiate an activity to run.
+var activity = new Sequence
+{
+    Activities =
+    {
+        new WriteLine("Hello World!"),
+        new WriteLine("We can do more than a one-liner!")
+    }
+};
+
+// Resolve a workflow runner to execute the activity.
+var workflowRunner = serviceProvider.GetRequiredService<IWorkflowRunner>();
+
+// Execute the activity.
+await workflowRunner.RunAsync(activity);
