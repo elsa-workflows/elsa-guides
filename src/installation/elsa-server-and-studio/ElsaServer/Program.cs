5327ab4f62f51b60e9bc4ca2864dce47404f50e3
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2024-01-20 17:16:39 +0100

    Add ElsaStudio and ElsaServer projects with initial setup
    The commit includes initial setup files for both ElsaStudio and ElsaServer projects. The ElsaStudio includes functioning web pages and routing system. The ElsaServer aims to load workflows and various scripts for enhanced functionalities. Relevant server logging configurations, base API URL and client configurations have also been set up.

---
+++ src/installation/elsa-server-and-studio/ElsaServer/Program.cs
@@ -0,0 +1,55 @@
+using Elsa.EntityFrameworkCore.Modules.Management;
+using Elsa.EntityFrameworkCore.Modules.Runtime;
+using Elsa.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.UseStaticWebAssets();
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services
+    .AddElsa(elsa => elsa
+        .UseIdentity(identity =>
+        {
+            identity.TokenOptions = options => options.SigningKey = "large-signing-key-for-signing-JWT-tokens";
+            identity.UseAdminUserProvider();
+        })
+        .UseDefaultAuthentication()
+        .UseWorkflowManagement(management => management.UseEntityFrameworkCore())
+        .UseWorkflowRuntime(runtime => runtime.UseEntityFrameworkCore())
+        .UseScheduling()
+        .UseJavaScript()
+        .UseLiquid()
+        .UseCSharp()
+        .UseHttp(http => http.ConfigureHttpOptions = options => configuration.GetSection("Http").Bind(options))
+        .UseWorkflowsApi()
+        .UseRealTimeWorkflows()
+        .AddActivitiesFrom<Program>()
+        .AddWorkflowsFrom<Program>()
+    );
+
+services.AddCors(cors => cors.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().WithExposedHeaders("*")));
+services.AddRazorPages(options => options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute()));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseRouting();
+app.UseCors();
+app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseWorkflowsApi();
+app.UseWorkflows();
+app.UseWorkflowsSignalRHubs();
+app.MapFallbackToPage("/_Host");
+app.Run();
