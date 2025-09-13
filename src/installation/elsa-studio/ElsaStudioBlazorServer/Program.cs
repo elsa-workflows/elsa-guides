4d0b4c61734d08773e3b8a4dd600680f67bd4afd
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2025-09-13 20:37:47 +0200

    Upgrade Elsa packages to v3.5.0 and enhance service configuration
    Updated Elsa-related package references to version 3.5.0. Refactored service configuration with `BackendApiConfig` and integrated `UseElsaIdentity` into the login module. Cleaned up unused content configuration.

--- src/installation/elsa-studio/ElsaStudioBlazorServer/Program.cs
+++ src/installation/elsa-studio/ElsaStudioBlazorServer/Program.cs
@@ -2,7 +2,9 @@ using Elsa.Studio.Core.BlazorServer.Extensions;
 using Elsa.Studio.Dashboard.Extensions;
 using Elsa.Studio.Extensions;
 using Elsa.Studio.Login.BlazorServer.Extensions;
+using Elsa.Studio.Login.Extensions;
 using Elsa.Studio.Login.HttpMessageHandlers;
+using Elsa.Studio.Models;
 using Elsa.Studio.Shell.Extensions;
 using Elsa.Studio.Workflows.Extensions;
 using Elsa.Studio.Workflows.Designer.Extensions;
@@ -22,10 +24,18 @@ builder.Services.AddServerSideBlazor(options =>
 // Register shell services and modules.
 builder.Services.AddCore();
 builder.Services.AddShell(options => configuration.GetSection("Shell").Bind(options));
-builder.Services.AddRemoteBackend(
-    elsaClient => elsaClient.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler),
-    options => configuration.GetSection("Backend").Bind(options));
-builder.Services.AddLoginModule();
+
+var backendApiConfig = new BackendApiConfig
+{
+    ConfigureBackendOptions = backendOptions => configuration.GetSection("Backend").Bind(backendOptions),
+    ConfigureHttpClientBuilder = elsaClient => elsaClient.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler)
+};
+
+builder.Services.AddRemoteBackend(backendApiConfig);
+builder.Services
+    .AddLoginModule()
+    .UseElsaIdentity();
+
 builder.Services.AddDashboardModule();
 builder.Services.AddWorkflowsModule();
 
